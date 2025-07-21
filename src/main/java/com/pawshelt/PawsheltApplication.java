fa8ea6d16972a2bcd6f5e69475cfeda00ade7a38
PabloDev96 <pablo.diazgar@gmail.com>
2025-07-21 14:56:36 +0200

    Primer commit - Proyecto PawShelt

---
+++ src/main/java/com/pawshelt/PawsheltApplication.java
@@ -0,0 +1,13 @@
+package com.pawshelt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawsheltApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PawsheltApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pawshelt/PawsheltApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pawshelt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawsheltApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
