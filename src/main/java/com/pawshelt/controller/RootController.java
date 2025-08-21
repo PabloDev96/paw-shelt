d8b9a984d7c8d0165d56f3247c269e95d454feb1
PabloDev96 <pablo.diazgar@gmail.com>
2025-08-21 09:15:30 +0200

    Create RootController.java

---
+++ src/main/java/com/pawshelt/controller/RootController.java
@@ -0,0 +1,14 @@
+package com.pawshelt.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/")
+    public ResponseEntity<String> root() {
+        return ResponseEntity.ok("PawShelt API is running");
+    }
+}
